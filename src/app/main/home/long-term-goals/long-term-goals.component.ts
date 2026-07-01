b190d2e8596392127b358269d8890f988a434a19
Arya Srinivasan <arssrini@ucsc.edu>
2026-07-01 11:12:25 -0700

    feat: addLong Term Goals Item implementation

--- src/app/main/home/long-term-goals/long-term-goals.component.ts
+++ src/app/main/home/long-term-goals/long-term-goals.component.ts
@@ -3,6 +3,7 @@ import { LongTermGoalsAnimations } from './long-term-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { LongTermGoalsItemComponent } from 'src/app/main/home/long-term-goals/long-term-goals-item/long-term-goals-item.component'
 
 @Component({
   selector: 'app-long-term-goals',
@@ -11,7 +12,7 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: LongTermGoalsAnimations,
   standalone: true,
-  imports: [
+  imports: [ LongTermGoalsItemComponent
   ],
 })
 export class LongTermGoalsComponent implements OnInit {

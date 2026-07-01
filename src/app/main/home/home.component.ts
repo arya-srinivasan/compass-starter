4a75aa0d4d3a17ab14d11a02c79284008e34f3fc
Arya Srinivasan <arssrini@ucsc.edu>
2026-07-01 10:11:11 -0700

    feat: addWeekly Goals Header implementation

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
+import { WeeklyGoalsComponent } from 'src/app/main/home/weekly-goals/weekly-goals.component';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
   standalone: true,
   animations: HomeAnimations,
   imports: [
+    WeeklyGoalsComponent,
     NavbarComponent,
   ]
 })

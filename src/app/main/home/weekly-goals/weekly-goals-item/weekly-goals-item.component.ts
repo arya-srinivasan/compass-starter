13da9bfaf0de1885961e5ca832f0fe7a19d2e053
aluo28 <aluo28@ucsc.edu>
2026-07-01 10:05:00 -0700

    feat: addWeekly Goals Modal implementation

--- src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
@@ -3,13 +3,6 @@ import { WeeklyGoalsItemAnimations } from './weekly-goals-item.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
-import { MatCheckbox } from '@angular/material/checkbox';
-import { WeeklyGoalData } from '../../home.model';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { WEEKLYGOAL_DB } from 'src/app/core/store/weekly-goal/weekly-goal.mock';
-import { WeeklyGoal } from 'src/app/core/store/weekly-goal/weekly-goal.model';
-import { HASHTAG_DB } from 'src/app/core/store/hashtag/hashtag.mock';
-import { Hashtag } from 'src/app/core/store/hashtag/hashtag.model';
 
 @Component({
   selector: 'app-weekly-goals-item',
@@ -18,7 +11,7 @@ import { Hashtag } from 'src/app/core/store/hashtag/hashtag.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsItemAnimations,
   standalone: true,
-  imports: [ MatCheckbox, MatProgressSpinner
+  imports: [
   ],
 })
 export class WeeklyGoalsItemComponent implements OnInit {
@@ -27,16 +20,11 @@ export class WeeklyGoalsItemComponent implements OnInit {
 
   /** The current signed in user. */
   currentUser: Signal<User> = this.authStore.user;
-  goal = input<WeeklyGoal>();
-  hashtag = input<Hashtag|undefined>();
 
   // --------------- LOCAL UI STATE ----------------------
 
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
-  // goalText: string = 'Apply to Microsoft';
-  // hashText: string = 'apply-internships';
-  //goal = input<WeeklyGoalData>();
 
   // --------------- COMPUTED DATA -----------------------
 
--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -3,13 +3,7 @@ import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
-import { WeeklyGoalsItemComponent } from './weekly-goals-item/weekly-goals-item.component';
-import { WeeklyGoalData } from 'src/app/main/home/home.model';
-import { Timestamp } from '@angular/fire/firestore';
-import { WEEKLYGOAL_DB } from 'src/app/core/store/weekly-goal/weekly-goal.mock';
-import { WeeklyGoal } from 'src/app/core/store/weekly-goal/weekly-goal.model';
-import { HASHTAG_DB } from 'src/app/core/store/hashtag/hashtag.mock';
-import { Hashtag } from 'src/app/core/store/hashtag/hashtag.model';
+import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component'
 
 @Component({
   selector: 'app-weekly-goals',
@@ -18,7 +12,8 @@ import { Hashtag } from 'src/app/core/store/hashtag/hashtag.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsAnimations,
   standalone: true,
-  imports: [ WeeklyGoalsItemComponent,
+  imports: [
+    WeeklyGoalsModalComponent,
   ],
 })
 export class WeeklyGoalsComponent implements OnInit {
@@ -27,42 +22,11 @@ export class WeeklyGoalsComponent implements OnInit {
 
   /** The current signed in user. */
   currentUser: Signal<User> = this.authStore.user;
-  /** weekly goal from mock.ts file. */
-  task: WeeklyGoal[] = WEEKLYGOAL_DB;
-  /** hashtag from mock.ts file. */
-  htg: Hashtag[] = HASHTAG_DB;
-  
+
   // --------------- LOCAL UI STATE ----------------------
 
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
-
-  /** hashtag map that maps WEEKLYGOAL_DB to HASHTAG_DB based on id */
-  hashtagMap = Object.fromEntries(
-    this.htg.map((h) => [h.__id, h]),
-  );
-
-  //sampleData: WritableSignal<WeeklyGoalData | null> = signal(null);
-  // sampleData: WeeklyGoalData = {
-  //   __id: 'wg1',
-  //   __userId: 'test-user',
-  //   __quarterlyGoalId: 'qg1',
-  //   __hashtagId: 'ht1',
-  //   text: 'Finish Google Cover Letter',
-  //   completed: false,
-  //   order: 1,
-  //   _createdAt: Timestamp.now(),
-  //   _updatedAt: Timestamp.now(),
-  //   _deleted: false,
-  //   hashtag: {
-  //     __id: 'ht1',
-  //     name: 'apply-internships',
-  //     color: '#EE8B72',
-  //     _createdAt: Timestamp.now(),
-  //     _updatedAt: Timestamp.now(),
-  //     _deleted: false,
-  //   },
-  // };
 
   // --------------- COMPUTED DATA -----------------------
 
@@ -77,30 +41,6 @@ export class WeeklyGoalsComponent implements OnInit {
 
   // --------------- LOAD AND CLEANUP --------------------
   
-  ngOnInit() {
-    // setTimeout(() => {
-    //   this.sampleData.set({
-    //     __id: 'wg1',
-    //     __userId: 'test-user',
-    //     __hashtagId: 'ht1',
-    //     __quarterlyGoalId: 'qg1',
-    //     text: 'Apply to Microsoft',
-    //     order: 1,
-    //     completed: false,
-    //     _createdAt: Timestamp.now(),
-    //     _updatedAt: Timestamp.now(),
-    //     _deleted: false,
-    //     hashtag: {
-    //       __id: 'ht1',
-    //       __userId: 'test-user',
-    //       name: 'apply-internships',
-    //       color: '#EE8B72',
-    //       _createdAt: Timestamp.now(),
-    //       _updatedAt: Timestamp.now(),
-    //       _deleted: false,
-    //     },
-    //   });
-    // }, 4000);
-    
+  ngOnInit(): void {
   }
 }

70edcca977cde76e75b47ec68b0ec583858bfbf3
Kaush Mendonca <kamendon@ucsc.edu>
2026-07-01 10:04:23 -0700

    feat: addWeekly Goals Item implementation

--- src/app/main/home/home.model.ts
+++ src/app/main/home/home.model.ts
@@ -1,1 +1,7 @@
 // Add any extra data types you'll need here!
+import { Hashtag } from '../../core/store/hashtag/hashtag.model';
+import { WeeklyGoal } from '../../core/store/weekly-goal/weekly-goal.model';
+
+export interface WeeklyGoalData extends WeeklyGoal {
+  hashtag: Hashtag;
+}
